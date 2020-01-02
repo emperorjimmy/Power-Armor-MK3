a9acd75d7705ac4170b6683e1d24517ad9b5e4a5
emperorjimmy <doughboy_slickplank@hotmail.com>
2020-01-02 15:42:40 +1100

    added settings
    Added disabling of individual items.

---
+++ data-updates.lua
@@ -0,0 +1,42 @@
+if settings.startup["pam3-nvm2"].value then
+ 	data.raw["item"]["night-vision-mk2-equipment"].enabled = false
+	data.raw["technology"]["night-vision-mk2-equipment"].enabled = false
+else
+	data.raw["item"]["night-vision-mk2-equipment"].enabled = true
+	data.raw["technology"]["night-vision-mk2-equipment"].enabled = true
+end
+if settings.startup["pam3-esm3"].value then
+ 	data.raw["item"]["energy-shield-mk3-equipment"].enabled = false
+	data.raw["technology"]["energy-shield-mk3-equipment"].enabled = false
+else
+	data.raw["item"]["energy-shield-mk3-equipment"].enabled = true
+	data.raw["technology"]["energy-shield-mk3-equipment"].enabled = true
+end
+if settings.startup["pam3-bm3"].value then
+ 	data.raw["item"]["battery-mk3-equipment"].enabled = false
+	data.raw["technology"]["battery-mk3-equipment"].enabled = false
+else
+	data.raw["item"]["battery-mk3-equipment"].enabled = true
+	data.raw["technology"]["battery-mk3-equipment"].enabled = true
+end
+if settings.startup["pam3-pnr"].value then
+ 	data.raw["item"]["nuclear-reactor-equipment"].enabled = false
+	data.raw["technology"]["nuclear-reactor-equipment"].enabled = false
+else
+	data.raw["item"]["nuclear-reactor-equipment"].enabled = true
+	data.raw["technology"]["nuclear-reactor-equipment"].enabled = true
+end
+if settings.startup["pam3-sin"].value then
+ 	data.raw["item"]["singularity-equipment"].enabled = false
+	data.raw["technology"]["singularity-equipment"].enabled = false
+else
+	data.raw["item"]["singularity-equipment"].enabled = true
+	data.raw["technology"]["singularity-equipment"].enabled = true
+end
+if settings.startup["pam3-pam4"].value then
+ 	data.raw["armor"]["power-armor-mk4"].enabled = false
+	data.raw["technology"]["power-armor-mk4"].enabled = false
+else
+	data.raw["armor"]["power-armor-mk4"].enabled = true
+	data.raw["technology"]["power-armor-mk4"].enabled = true
+end
---
+++ settings.lua
@@ -0,0 +1,44 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "pam3-nvm2",
+        setting_type = "startup",
+        default_value = false,
+		per_user = false
+    }, 
+    {
+        type = "bool-setting",
+        name = "pam3-esm3",
+        setting_type = "startup",
+        default_value = false,
+		per_user = false
+    }, 
+    {
+        type = "bool-setting",
+        name = "pam3-bm3",
+        setting_type = "startup",
+        default_value = false,
+		per_user = false
+    },
+    {
+        type = "bool-setting",
+        name = "pam3-pnr",
+        setting_type = "startup",
+        default_value = false,
+		per_user = false
+    }, 
+	{
+        type = "bool-setting",
+        name = "pam3-sin",
+        setting_type = "startup",
+        default_value = false,
+		per_user = false
+    },
+    {
+        type = "bool-setting",
+        name = "pam3-pam4",
+        setting_type = "startup",
+        default_value = false,
+		per_user = false
+    }
+})
