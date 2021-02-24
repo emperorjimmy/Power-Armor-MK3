2a6617d13caf5635e1517dbb3f32c769d7783cc0
emperorjimmy <doughboy_slickplank@hotmail.com>
2021-02-24 15:28:26 +1100

    Add files via upload

--- settings.lua
+++ settings.lua
@@ -1,51 +1,99 @@
-data:extend({
+data:extend
+{
     {
         type = "bool-setting",
         name = "pam3-nvm2",
         setting_type = "startup",
         default_value = false,
-		per_user = false
+		per_user = false,
+        order = "a1"
     }, 
     {
         type = "bool-setting",
         name = "pam3-esm3",
         setting_type = "startup",
         default_value = false,
-		per_user = false
+		per_user = false,
+        order = "a2"
     }, 
     {
         type = "bool-setting",
         name = "pam3-bm3",
         setting_type = "startup",
         default_value = false,
-		per_user = false
+		per_user = false,
+        order = "a3"
     },
     {
         type = "bool-setting",
         name = "pam3-pnr",
         setting_type = "startup",
         default_value = false,
-		per_user = false
+		per_user = false,
+        order = "a4"
     }, 
 	{
         type = "bool-setting",
         name = "pam3-sin",
         setting_type = "startup",
         default_value = false,
-		per_user = false
+		per_user = false,
+        order = "a5"
     },
     {
         type = "bool-setting",
         name = "pam3-pam4",
         setting_type = "startup",
         default_value = false,
-		per_user = false
+		per_user = false,
+        order = "a6"
+    },
+    {
+        type = "bool-setting",
+        name = "pam3-p4xl",
+        setting_type = "startup",
+        default_value = false,
+		per_user = false,
+        order = "a7"
+    },
+    {
+        type = "int-setting",
+        name = "pam3-p4gw",
+        setting_type = "startup",
+        default_value = 20,
+        minimum_value = 1,
+        order = "a8"
+    },
+    {
+        type = "int-setting",
+        name = "pam3-p4gh",
+        setting_type = "startup",
+        default_value = 20,
+        minimum_value = 1,
+        order = "a9"
     },
     {
         type = "bool-setting",
         name = "pam3-sgxl",
         setting_type = "startup",
         default_value = false,
-		per_user = false
+		per_user = false,
+        order = "b1"
+    },
+    {
+        type = "int-setting",
+        name = "pam3-sgw",
+        setting_type = "startup",
+        default_value = 10,
+        minimum_value = 1,
+        order = "b2"
+    },
+    {
+        type = "int-setting",
+        name = "pam3-sgh",
+        setting_type = "startup",
+        default_value = 6,
+        minimum_value = 1,
+        order = "b3"
     }
-})
+}
